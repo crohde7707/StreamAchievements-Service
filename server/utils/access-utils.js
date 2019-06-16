0d75aaa3a180a5cd0775d107afb31f3e6993781e
Chris Rohde <crohde7707@gmail.com>
2019-06-16 13:01:57 -0500

    Add content-type header

--- server/utils/access-utils.js
+++ server/utils/access-utils.js
@@ -7,6 +7,7 @@ const allowAccess = async (req, res, next) => {
 	}
 
 	res.header('Access-Control-Allow-Credentials', true);
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 
 	next();
 }
