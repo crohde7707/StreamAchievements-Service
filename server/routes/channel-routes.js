fa327902689770d4274d0368bdf63aff78f33f21
Chris Rohde <crohde7707@gmail.com>
2019-10-01 06:39:57 -0500

    Retire gatekeep

--- server/routes/channel-routes.js
+++ server/routes/channel-routes.js
@@ -985,20 +985,57 @@ router.post("/signup", isAuthorized, (req, res) => {
 	//generate code
 	let uid = req.body.uid;
 
-	Token.findOne({uid}).then(foundToken => {
-		if(foundToken) {
-			res.json({
-				error: "You have already signed up!"
-			});
-		} else {
-			let token = new Token({uid: req.user._id, token: 'not issued'});
+	let token = new Token({uid: req.user._id, token: 'not issued'});
 
-			token.save().then(savedToken => {
-				res.json({
-					signup: true
-				});
+	let generatedToken = crypto.randomBytes(16).toString('hex');
+	
+	token.token = generatedToken;
+	token.created = Date.now();
+	token.save().then(savedToken => {
+
+		let email = req.user.email;
+
+		var auth = {
+		    type: 'oauth2',
+		    user: process.env.GML,
+		    clientId: process.env.GMLCID,
+		    clientSecret: process.env.GMLCS,
+		    refreshToken: process.env.GMLRT
+		};
+
+		var transporter = nodemailer.createTransport({
+			service: 'gmail',
+			auth: auth
+		});
+
+		const mailOptions = {
+		    from: process.env.GML, // sender address
+		    to: email, // list of receivers
+		    subject: 'Your Confirmation Code!', // Subject line
+		    html: '<div style="background:#222938;padding-bottom:30px;"><h1 style="text-align:center;background:#2f4882;padding:15px;margin-top:0;"><img style="max-width:600px;" src="https://res.cloudinary.com/phirehero/image/upload/v1557947921/sa-logo.png" /></h1><h2 style="color:#FFFFFF; text-align: center;margin-top:30px;margin-bottom:25px;font-size:22px;">Thank you for your interest in Stream Achievements!</h2><p style="color:#FFFFFF;font-weight:bold;font-size:16px; text-align: center;">You are ready to start creating achievements that your community will be able to hunt for and earn!</p><p style="color:#FFFFFF;font-weight:bold;font-size:16px; text-align: center;">To get started, all you need to do is <a style="color: #ecdc19;" href="http://streamachievements.com/channel/verify?id=' + generatedToken + '&utm_medium=Email">verify your account</a>, and you\'ll be all set!</p><p style="color:#FFFFFF;font-weight:bold;font-size:16px; text-align: center;">We are truly excited to see what you bring in terms of achievements, and can\'t wait to see how much your community engages and enjoys them!</p></div>'
+		};
+
+		transporter.sendMail(mailOptions, function (err, info) {
+		   if(err)
+		     console.log(err)
+		   else
+
+		   	new Notice({
+				user: req.user._id,
+				logo: DEFAULT_ICON,
+				message: "Your channel is ready to begin! Go check your email for your confirmation code, and don't forget to check your spam folder!",
+				date: Date.now(),
+				type: 'confirmation',
+				status: 'new'
+			}).save().then(savedNotice => {
+				console.log(savedNotice);
 			});
-		}
+
+		    res.json({
+		    	message: "email sent"
+		    });
+		});
+
 	});
 });
 
